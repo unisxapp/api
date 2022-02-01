199710a3b8babfaccc778c9902e44cf9a07eed20
Dmitry Vasilev <dmitry.vasil@gmail.com>
2022-02-01 18:18:46 +0800

    deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+SSH_HOST=$1
+
+cd $(dirname $0)
+
+if [ -z $SSH_HOST ] ; then
+  echo SSH_HOST is not specified
+  exit 1
+fi
+
+echo 'Install nginx'
+ssh $SSH_HOST sudo bash -e <<-EOF
+  if ! which nginx ; then
+    apt-get -y install nginx
+  fi
+EOF
+
+echo 'Create dir'
+ssh $SSH_HOST sudo bash -e <<-EOF
+  mkdir -p /opt/api
+EOF
+
+echo 'Copy files'
+rsync -r --rsync-path='sudo rsync' ./docker-compose.yml api.conf $SSH_HOST:/opt/api
+
+ssh $SSH_HOST bash -e <<-EOF
+  cd /opt/api
+  echo 'Reload nginx'
+  sudo cp /opt/api/api.conf /etc/nginx/sites-available
+  sudo ln -s /etc/nginx/sites-{available,enabled}/api.conf
+  sudo nginx -s reload
+  echo 'Run docker compose'
+  docker-compose up -d
+EOF
